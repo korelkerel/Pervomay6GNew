4474e3e745c8d4a4648c88a3df5831fa9e0f6966
korelkerel <chrnov2002@gmail.com>
2024-10-29 19:42:34 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -47,9 +47,3 @@ app.post('/api/chat', async (req, res) => {
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
-
-
-app.listen(PORT, () => {
-    console.log(`Сервер запущен на порту ${PORT}`);
-});
-
