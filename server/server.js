b917c888fe7e08c158cbb13424109e77fd8fadfd
korelkerel <chrnov2002@gmail.com>
2024-10-29 22:32:53 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.post('/api/chat', async (req, res) => {
     const apiKey = process.env.HUGGING_FACE_API_KEY;
 
     try {
-        const response = await fetch('https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill', {
+        const response = await fetch('https://api-inference.huggingface.co/models/facebook/blenderbot-1B-distill', {
             method: 'POST',
             headers: {
                 'Authorization': `Bearer ${apiKey}`,
