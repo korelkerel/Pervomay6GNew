edf35c3ed665dbf6cb9cc556ef7f04c670245a81
korelkerel <chrnov2002@gmail.com>
2024-11-25 21:46:54 +0300

    Add files via upload

---
+++ docs/scripts2.js
@@ -0,0 +1,11 @@
+document.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) { // Проверяем, нажата ли клавиша Enter
+        const messageInput = document.getElementById('messageInput'); // Поле для ввода сообщения
+        const userMessageText = messageInput.value.trim(); // Получаем текст сообщения
+
+        if (userMessageText !== '') {
+            document.getElementById('sendBtn').click(); // Имитируем нажатие кнопки отправки
+            messageInput.value = ''; // Очищаем поле ввода после отправки
+        }
+    }
+});
